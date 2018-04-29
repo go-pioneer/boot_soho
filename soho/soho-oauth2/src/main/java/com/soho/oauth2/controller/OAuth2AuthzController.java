9a86c1340646b6c2c245d103621c3c16d62fe144
chenjianqiang <chenjianqiang@192.168.1.251>
2018-04-29 12:12:15 +0800

    1.优化OAUTH2.0接口实例化结构

--- soho/soho-oauth2/src/main/java/com/soho/oauth2/controller/OAuth2AuthzController.java
+++ soho/soho-oauth2/src/main/java/com/soho/oauth2/controller/OAuth2AuthzController.java
@@ -2,9 +2,10 @@ package com.soho.oauth2.controller;
 
 import com.soho.mybatis.exception.BizErrorEx;
 import com.soho.oauth2.service.OAuth2AuthzService;
-import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,28 +14,41 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Created by shadow on 2017/5/2.
  */
-public abstract class OAuth2AuthzController {
 
-    protected abstract OAuth2AuthzService getOAuth2AuthzService();
+@Controller
+@RequestMapping("/oauth2.0/v1.0")
+public class OAuth2AuthzController {
 
+    @Autowired(required = false)
+    private OAuth2AuthzService oAuth2AuthzService;
+
+    @RequestMapping(value = {"/authorize"})
     public Object authorize(HttpServletRequest request, HttpServletResponse response) throws BizErrorEx {
-        return getOAuth2AuthzService().authorize(request, response);
+        return oAuth2AuthzService.authorize(request, response);
     }
 
+    @ResponseBody
+    @RequestMapping(value = {"/access_token"}, method = {RequestMethod.POST})
     public Object access_token(HttpServletRequest request, HttpServletResponse response) throws BizErrorEx {
-        return getOAuth2AuthzService().access_token(request, response);
+        return oAuth2AuthzService.access_token(request, response);
     }
 
+    @ResponseBody
+    @RequestMapping(value = {"/userinfo"}, method = {RequestMethod.POST})
     public Object userinfo(HttpServletRequest request, HttpServletResponse response) throws BizErrorEx {
-        return getOAuth2AuthzService().userinfo(request, response);
+        return oAuth2AuthzService.userinfo(request, response);
     }
 
+    @ResponseBody
+    @RequestMapping(value = {"/logout"}, method = {RequestMethod.POST})
     public Object logout(HttpServletRequest request, HttpServletResponse response) throws BizErrorEx {
-        return getOAuth2AuthzService().logout_token(request, response);
+        return oAuth2AuthzService.logout_token(request, response);
     }
 
+    @ResponseBody
+    @RequestMapping(value = {"/refresh_token"}, method = {RequestMethod.POST})
     public Object refresh_token(HttpServletRequest request, HttpServletResponse response) throws BizErrorEx {
-        return getOAuth2AuthzService().refresh_token(request, response);
+        return oAuth2AuthzService.refresh_token(request, response);
     }
 
 }
