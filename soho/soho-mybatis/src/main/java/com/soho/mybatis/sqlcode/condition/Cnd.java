6100b99cbd3a7f0f4ce6664ced8a75fe5d88ba07
chenjianqiang <chenjianqiang@192.168.1.251>
2018-07-19 12:57:55 +0800

    1.增加cnd接口注释

--- soho/soho-mybatis/src/main/java/com/soho/mybatis/sqlcode/condition/Cnd.java
+++ soho/soho-mybatis/src/main/java/com/soho/mybatis/sqlcode/condition/Cnd.java
@@ -9,70 +9,100 @@ import java.util.List;
 
 public interface Cnd {
 
+    // =
     public Cnd eq(String key, Object value);
 
+    // <>
     public Cnd noteq(String key, Object value);
 
+    // <
     public Cnd lt(String key, Object value);
 
+    // <=
     public Cnd lte(String key, Object value);
 
+    // >
     public Cnd gt(String key, Object value);
 
+    // >=
     public Cnd gte(String key, Object value);
 
+    // is null
     public Cnd isnull(String key);
 
+    // is not null
     public Cnd notnull(String key);
 
+    // between x and y
     public Cnd between(String key, Object value1, Object value2);
 
+    // not between x and y
     public Cnd notbetween(String key, Object value1, Object value2);
 
+    // in
     public Cnd in(String key, Object... values);
 
+    // in
     public Cnd in(String key, Object values);
 
+    // not in
     public Cnd notin(String key, Object... values);
 
+    // not in
     public Cnd notin(String key, Object values);
 
+    // like
     public Cnd like(String key, Object value);
 
+    // not like
     public Cnd notlike(String key, Object value);
 
+    // or
     public Cnd or(Cnd... cnds);
 
+    // 分页方法(根据数据库方言)
     public Cnd limit(Integer pageNo, Integer pageSize);
 
+    // 分页方法(根据数据库方言)
     public Cnd limit(Integer pageNo, Integer pageSize, boolean spilled);
 
+    // 分页方法(根据数据库方言)
     public Cnd limit(Integer pageSize);
 
+    // distinct
     public Cnd distinct(String... keys);
 
+    // group by
     public Cnd groupby(String... keys);
 
+    // order by
     public Cnd orderby(String key, SortBy sortBy);
 
+    // 获取指定字段
     public Cnd fields(String... keys);
 
-    public Cnd addUpdateObj(Object obj);
-
+    // 指定更新字段
     public Cnd addUpdateKeyValue(String[] keys, Object... values);
 
+    // 指定更新对象(自定义sql时使用)
+    public Cnd addUpdateObj(Object obj);
+
+    // 传递额外参数(自定义sql时使用)
     public Cnd addOther(String key, Object value);
+
+    // from table1 a(连表查询使用)
+    public Cnd from(String table);
+
+    // left(right,inner) join table1 b(连表查询使用)
+    public Cnd join(Join join);
+
+    // 复制条件
+    public Cnd copy(Cnd cnd);
 
     public List<Condition<?>> getConditions();
 
     public Pagination<Object> getPagination();
 
     public void setPagination(Pagination<Object> pagination);
-
-    public Cnd from(String table);
-
-    public Cnd join(Join join);
-
-    public Cnd copy(Cnd cnd);
 
 }
