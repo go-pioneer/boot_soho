fe03d910925905126437001234093d6dd65a3c44
chenjianqiang <124010356@qq.com>
2018-09-16 15:41:19 +0800

    1.实现asyncontext异步处理业务并监控结果

--- soho/soho-demo/src/main/java/com/soho/demo/controller/DogController.java
+++ soho/soho-demo/src/main/java/com/soho/demo/controller/DogController.java
@@ -6,11 +6,23 @@ import com.soho.mybatis.exception.BizErrorEx;
 import com.soho.mybatis.sqlcode.condition.imp.SQLCnd;
 import com.soho.spring.cache.annotation.RDLock;
 import com.soho.spring.model.ReqData;
+import com.soho.spring.model.RetData;
+import com.soho.spring.utils.HttpUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import javax.servlet.AsyncContext;
+import javax.servlet.AsyncEvent;
+import javax.servlet.AsyncListener;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/dog")
@@ -67,6 +79,80 @@ public class DogController {
         return FileUtils.uploadImageByReSize(file, 500, 500, "1", true);
     }*/
 
+    private static Map<String, AsyncContext> map = new HashMap<>();
+    public static String nonce = "test";
+
+    @ResponseBody
+    @RequestMapping("/test1")
+    public void test1(HttpServletRequest request, HttpServletResponse response) throws BizErrorEx {
+        AsyncContext context = map.get(nonce);
+        HttpUtils.responseJsonData((HttpServletResponse) context.getResponse(), new RetData<>(new HashMap<>()));
+        context.complete();
+    }
+
+    @ResponseBody
+    @RequestMapping("/test")
+    public void test(HttpServletRequest request, HttpServletResponse response) throws BizErrorEx {
+        AsyncContext context = request.startAsync();
+        context.setTimeout(10000);
+        context.addListener(new AsyncListener() {
+            @Override
+            public void onComplete(AsyncEvent asyncEvent) throws IOException {
+                System.out.println("---已完成处理");
+                Object nonce = asyncEvent.getAsyncContext().getRequest().getAttribute(DogController.nonce);
+                if (nonce != null && map.get(nonce) != null) {
+                    map.remove(nonce);
+                    System.out.println("---删除持久化context: " + nonce);
+                }
+            }
+
+            @Override
+            public void onTimeout(AsyncEvent asyncEvent) throws IOException {
+                System.out.println("---onTimeout");
+                AsyncContext asyncContext = asyncEvent.getAsyncContext();
+                HttpUtils.responseJsonData((HttpServletResponse) asyncContext.getResponse(), new RetData<>("onTimeout"));
+                asyncContext.complete();
+            }
+
+            @Override
+            public void onError(AsyncEvent asyncEvent) throws IOException {
+                System.out.println("---onError");
+                AsyncContext asyncContext = asyncEvent.getAsyncContext();
+                HttpUtils.responseJsonData((HttpServletResponse) asyncContext.getResponse(), new RetData<>("onError"));
+                asyncContext.complete();
+            }
+
+            @Override
+            public void onStartAsync(AsyncEvent asyncEvent) throws IOException {
+                System.out.println("---onStartAsync");
+            }
+        });
+        map.put(nonce, context);
+        new Thread(new Runnable() {
+            int i = 0;
+
+            @Override
+            public void run() {
+                request.setAttribute(nonce, nonce);
+                while (true) {
+                    System.out.println(request.isAsyncStarted() + "---");
+                    if (!request.isAsyncStarted()) {
+                        System.out.println("---退出循环检测");
+                        return;
+                    }
+                    i++;
+                    try {
+                        Thread.sleep(200);
+                        System.out.println("---" + i);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+
     @ResponseBody
     @RequestMapping("/save")
     public Object save() throws BizErrorEx {
@@ -100,14 +186,59 @@ public class DogController {
 
     @ResponseBody
     @RequestMapping("/webflux")
-    public Mono<Dog> webflux() throws BizErrorEx {
+    public Flux<Dog> webflux() {
+        System.err.println("findAllCity 1");
+        Flux<Dog> flux = Flux.create(dogFluxSink -> {
+            try {
+                dogService.findAll().forEach(dog -> dogFluxSink.next(dog));
+                // dogFluxSink.next(dogService.findById(1l));
+            } catch (BizErrorEx ex) {
+                ex.printStackTrace();
+            }
+            dogFluxSink.complete();
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.err.println("findAllCity 2");
+        });
+        System.err.println("findAllCity  end 3");
+        return flux;
+
+        /*Dog dog = new Dog();
+        dog.setName("小狗");
+        dog.setAge(5);
+        dog.setSex(1);
+        dog.setCtime(System.currentTimeMillis());
+        dog.setState(1);
+        return Mono.just(dog);*/
+    }
+
+    @ResponseBody
+    @RequestMapping("/webflux2")
+    public Mono<Dog> save1(String id) throws BizErrorEx {
+        System.err.println("findOneCity 1" + id);
         Dog dog = new Dog();
         dog.setName("小狗");
         dog.setAge(5);
         dog.setSex(1);
         dog.setCtime(System.currentTimeMillis());
         dog.setState(1);
-        return Mono.just(dog);
+        dogService.insert(dog);
+        Mono mono = Mono.create(cityMonoSink -> {
+            try {
+                Dog object = dogService.findById(dog.getId());
+                cityMonoSink.success(object);
+                System.err.println("findOneCity 2");
+                Integer.parseInt("s");
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                cityMonoSink.error(ex);
+            }
+        });
+        System.err.println("findOneCity 4");
+        return mono;
     }
 
     int i = 0;
